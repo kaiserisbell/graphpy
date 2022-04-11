19b05818047fd05bf951ef0af64bfa649d5e5e18
Kaiser <kaiserisbell96@gmail.com>
2022-04-11 07:11:05 -0400

    Update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from random import randint
 
 for i in range(1, 365):
-    d = str(i) + ' days ago'
+    d = str(i) + ' days ago '
     with open('file.txt', 'a') as file:
         file.write(d)
         os.system('git-add-.')
