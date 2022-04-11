a1ac99c918484f197ed7f21ec3403c21b317dfad
Kaiser <kaiserisbell96@gmail.com>
2022-04-11 07:14:55 -0400

    Update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ for i in range(1, 365):
     d = str(i) + ' days ago '
     with open('file.txt', 'a') as file:
         file.write(d)
-        os.system('git-add-.')
+        os.system('git add .')
         os.system('git commit --date="' + d + '" -m "Commit"')
 
 
